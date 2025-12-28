cb7c4a296f185911a71686ef0f2db0375fcbe06a
Mirodil Mirsadikov <mirsadikovmirodil5610056@gmail.com>
2025-12-29 00:12:40 +0500

    update

--- src/module/part_service/model/part_model.go
+++ src/module/part_service/model/part_model.go
@@ -15,8 +15,8 @@ type Part struct {
 	DeviceId    int64                    `json:"device_id" gorm:"not null;index"`
 	SupplierId  int64                    `json:"supplier_id" gorm:"not null;index"`
 	MasterId    int64                    `json:"master_id" gorm:"not null;index"`
-	IncomePrice float64                  `json:"income_price" gorm:"type:decimal(10,2);not null"`
-	Price       float64                  `json:"price" gorm:"type:decimal(10,2);not null"`
+	IncomePrice float64                  `json:"income_price" gorm:"type:decimal(10,2)"`
+	Price       float64                  `json:"price" gorm:"type:decimal(10,2)"`
 	CreatedAt   *time.Time               `json:"created_at" gorm:"autoCreateTime"`
 	DeletedAt   *gorm.DeletedAt          `json:"deleted_at" gorm:"index"`
 	Device      *device_model.Device     `json:"device,omitempty" gorm:"foreignKey:DeviceId;references:ID"`
